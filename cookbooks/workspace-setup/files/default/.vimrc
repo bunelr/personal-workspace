f0529b163dd2ea23e49a4b226d3f8096c382c0a4
Rudy Bunel <bunel.rudy@gmail.com>
2014-04-06 22:51:01 -0300

    Provision| Use Vundle to install vim plugin
    Installation of NERDTree

--- cookbooks/workspace-setup/files/default/.vimrc
+++ cookbooks/workspace-setup/files/default/.vimrc
@@ -1,3 +1,14 @@
+set nocompatible
+filetype off
+set rtp+=~/.vim/bundle/vundle/
+call vundle#rc()
+
+Plugin 'gmarik/vundle'
+Plugin 'scrooloose/nerdtree'
+
+filetype plugin indent on
+
+
 set autoindent
 set noexpandtab
 set tabstop=4
--- cookbooks/workspace-setup/recipes/vim-setup.rb
+++ cookbooks/workspace-setup/recipes/vim-setup.rb
@@ -16,3 +16,23 @@ cookbook_file "/home/vagrant/.vimrc" do
   source ".vimrc"
   mode 0644
 end
+
+directory "/home/vagrant/.vim/bundle/" do
+  recursive true
+  action :create
+  mode 0777
+end
+
+git "Vundle" do
+  repository 'https://github.com/gmarik/vundle.git'
+  reference 'master'
+  action :checkout
+  destination '/home/vagrant/.vim/bundle/vundle'
+end
+
+execute "Install Vim plugin with Vundle" do
+  user "vagrant"
+  group "vagrant"
+  environment 'HOME' => "/home/vagrant/"
+  command "vim -c 'set shortmess=at' +PluginInstall +qall"
+end
